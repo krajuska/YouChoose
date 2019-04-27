48df47b5e114cd3c1d743eac1d11276c9d3d9772
Aline Krajuska <krajuska@gmail.com>
2019-04-26 21:51:47 -0300

    adds the new project - now with a working YouTube player in it

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'YouChooseApp' do
+  use_frameworks!
+
+  pod 'YoutubePlayerView', '~> 1.0.2'
+
+end
---
+++ Pods/Target Support Files/Pods-YouChooseApp/Pods-YouChooseApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YouChooseApp : NSObject
+@end
+@implementation PodsDummy_Pods_YouChooseApp
+@end
