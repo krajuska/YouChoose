1d80da5ce07213a7e92929a50eeca85330aaeb30
Aline Krajuska <krajuska@gmail.com>
2019-05-13 19:24:10 -0300

    begins implementing the main view -- absolutely not done yet

--- YouChooseApp/Controller/MainViewController.swift
+++ YouChooseApp/Controller/MainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class MainViewController: UIViewController {
 
@@ -16,15 +17,8 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        setHeader(self)
     }
-    */
 
 }
